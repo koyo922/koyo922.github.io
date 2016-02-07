d9a315ee46c1fed03d83030185ab3d6c0154c13b
QIAN Weishuo <koyo922@qq.com>
2016-02-07 10:20:24 +0900

    add exif-rotate

--- img/in-post/rotate.py
+++ img/in-post/rotate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from PIL import Image
+from PIL import Image #注意写法
 import exifread
 
 import sys
@@ -9,7 +9,7 @@ import os.path
 
 IMAGE_EXTENSIONS = ["jpg", "png", "gif"]
 
-os.chdir(sys.argv[1])
+os.chdir(sys.argv[1]) #用第一个命令行参数指定待处理照片所在的目录
 for filename in os.listdir():
 	if not os.path.splitext(filename)[1][1:].lower() in IMAGE_EXTENSIONS:
 		continue
@@ -19,5 +19,5 @@ for filename in os.listdir():
 
 	print(tags['Image Orientation'].printable)
 	if 'Rotated 90' in tags['Image Orientation'].printable:
-		print(filename)
+		print("done with:\t"+filename)
 		Image.open(filename).transpose(Image.ROTATE_90).save(filename)
