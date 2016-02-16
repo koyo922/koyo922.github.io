49de6ccfb966c51797dadf2a7051a8e58d3ab601
QIAN Weishuo <koyo922@qq.com>
2016-02-16 20:54:45 +0900

    add gsed-for-markdown-on-mac

--- share/patch_time_friend.sh
+++ share/patch_time_friend.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sed -Ei ".backup" \
+gsed -i.backup \
 	-e "s/电活/电话/g" \
-	-e "s/使  用/使用/g" \
+	-Ee ":a;N;$bash;s/使\s{2,}\n\s*用/使用/g" \
 	-e "s/使<br ?\/?>用/使用/g" \
 	$1
 
-echo "========== the diff START =========="
-diff $1.backup $1
-echo "========== the diff END =========="
+echo "========== diff START =========="
+colordiff -u $1.backup $1
+echo "========== diff END =========="
 
 echo "========== you can run 'rm -rf $1.backup' after confirm it =========="
